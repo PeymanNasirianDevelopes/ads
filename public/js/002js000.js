f44a42566d9d32d792be274074b6475929cef822
peyman313-web <peymannasirian1373@gmail.com>
2020-05-27 03:48:57 +0430

    Setup Project

---
+++ public/js/002js000.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // $('select').formSelect();
+    // $('select').material_select();
+//    $(".select2").select2({
+//        dir: "rtl"
+//    });
+    $('.sidenav').sidenav();
+ 
+});    $(".dropdown-trigger").dropdown();
---
+++ public/js/003js000.js
@@ -0,0 +1,11 @@
+ var params = {
+            language:'fa',
+            underGraduate: 'UnderGraduate',
+            diploma:'Diploma',
+            culture: 'fa',
+            choosePhoto: 'انتخاب عکس',
+            photoId: 'Photo',
+            changePhoto: 'تغییر عکس',
+            cultureVar: 'fa',
+            minAverageSalaryId: 'RecruitmentInfo_MinAverageSalary',
+        };
